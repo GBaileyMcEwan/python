bd7e0973d6fab18598a7f1d0934508b9e5d14ee8
Gordon Bailey-McEwan <gordon@Setups-iMac.local>
2019-11-28 16:00:22 +0200

    Added generate_receipts script

---
+++ src/receipts/gen_receipts.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+import random
+import os
+import json
+
+count = int(os.getenv("FILE_COUNT") or 10)
+words = [word.strip() for word in open('/usr/share/dict/words').readlines()]
+
+for identifier in range(count):
+    amount = random.uniform(1.0, 1000)
+    content = {
+        'topic': random.choice(words),
+        'value': "%.2f" % amount
+    }
+    with open(f'./new/receipt-{identifier}.json', 'w') as f:
+        json.dump(content, f)
