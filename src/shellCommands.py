50902fad75ab47df7277bce2db11d6c67ec23124
Gordon Bailey-McEwan <gordon@Setups-iMac.local>
2019-11-28 14:35:24 +0200

    Modified shellCommands

--- src/shellCommands.py
+++ src/shellCommands.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
+import subprocess
 
+#if the return code is 0, the command executed successfully!
+proc = subprocess.run(["ls","-l"])
+#print(f"{proc}")
