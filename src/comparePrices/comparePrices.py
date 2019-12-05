9a810d1b7f9468c5eb51243440eb918a0da0cad0
Gordon Bailey-McEwan <gordon@Setups-iMac.local>
2019-12-05 15:41:55 +0200

    updated comparePrices.py

--- src/comparePrices/comparePrices.py
+++ src/comparePrices/comparePrices.py
@@ -25,15 +25,42 @@ woolies = requests.get(f"https://www.woolworths.co.za/server/searchCategory?page
 
 #print the HTTP response
 #print(f"{woolies.text}")
+
+#Woolworths section
 myWoolworthsJSON = json.loads(woolies.text)
-print(f"{myWoolworthsJSON}")
+
+myCount=0
+myProductData = []
+with open(f'./wooliesPrices.json', 'w+') as f:
+	myProductData = []
+	for x in myWoolworthsJSON['contents']:
+		#print(f"{x}")
+		for y in x['mainContent']:
+				for z in y['contents']:
+					for a in z['records']:
+						myDict = a['attributes']
+						myDict2 = a['startingPrice']
+						if "p_displayName" in myDict.keys():
+							print(f"{myDict['p_displayName']} - Price: {myDict2['p_pl10']}")
+
+							content = {
+								'product': myDict['p_displayName'],
+								'price': myDict2['p_pl10']
+								}
+							myProductData.append(content)
+							myCount+=1
+	json.dump(myProductData, f)
 
 
+
+
+
+#PicknPay section
 #load the data into BS4
 soup = BeautifulSoup(pnp.text, 'html.parser')
 soup2 = BeautifulSoup(woolies.text, 'html.parser')
 #print(soup2)
-exit()
+#exit()
 
 # get data simply by looking for each h4 header (which contains the username)
 data = []
@@ -61,45 +88,6 @@ with open(f'./pnpPrices.json', 'w+') as f:
 			'price': data2[myCount]
 			}
 		print(content)
-		#myData = str(content).","
-		#print(myData)
-		myProductData.append(content)
-		myCount+=1
-		#json.dump(content, f)
-	json.dump(myProductData, f)
-
-
-
-############# Woolies section ##############
-
-# get data simply by looking for each h4 header (which contains the username)
-data = []
-for a in soup2.find_all('a', { 'class': 'range--title'}):
-	data.append(a.text)
-	#print(f"A RECORD: {a}")
-
-# get the comments of each user by looking for a div with class bbWrapper
-data2 = []
-for strong in soup2.find_all('strong', { 'class': 'price' }):
-	#remove all data with <div> or </div> or </span> or with spaces
-	#strong = re.sub(r'(^<div.*>|</div.*>|</span>|\s)', '', str(div))
-	#replace <span> with a . (pnps website has a span element separating rands and cents!)
-	#div = re.sub(r'<span>', '.', str(div))
-	data2.append(strong.text)
-	#print("FOUND STRONG")
-
-myCount=0
-with open(f'./wooliesPrices.json', 'w+') as f:
-	myProductData = []
-	#myProductData['picknpay'] = []
-	for product in data:
-		#print(colored(f"Product: {product}", 'red'))
-		#print(colored(f"Price: {data2[myCount]}", 'green'))
-		content = {
-			'product': product,
-			'price': data2[myCount]
-			}
-		#print(content)
 		#myData = str(content).","
 		#print(myData)
 		myProductData.append(content)
